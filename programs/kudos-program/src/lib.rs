1bae0608a73e58084844253dce4812e9b9ec9edb
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-08-09 18:15:36 +0800

    Looks like anchor test is working.

--- programs/kudos-program/src/lib.rs
+++ programs/kudos-program/src/lib.rs
@@ -6,10 +6,66 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod kudos_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_user_stats(ctx: Context<CreateUserStats>, name: String, pda_bump: u8) -> Result<()> {
+        let user_stats = &mut ctx.accounts.user_stats;
+        
+        if name.as_bytes().len() > 200 {
+            panic!();
+        }
+
+        user_stats.name = name;
+        user_stats.kudos = 0;
+        user_stats.bump = pda_bump;
+        
+        Ok(())
+    }
+
+    pub fn give_kudos(ctx: Context<GiveKudos>, amount: u64) -> Result<()> {
+        ctx.accounts.user_stats.kudos += amount;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct UserStats {
+    name: String,
+    kudos: u64,
+    bump: u8
+}
+
+#[derive(Accounts)]
+pub struct CreateUserStats<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    
+    // Space: 8 discriminator + 64 name + 8 kudos + 1 bump
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 64 + 8 + 1,
+        seeds = [b"user-stats", user.key().as_ref()],
+        bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct GiveKudos<'info> {
+    #[account(mut)]
+    pub kudos_sender: Signer<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub kudos_receiver: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"user-stats", kudos_receiver.key().as_ref()],
+        bump = user_stats.bump
+    )]
+    pub user_stats: Account<'info, UserStats>,
+}
--- tests/kudos-program.ts
+++ tests/kudos-program.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { KudosProgram } from "../target/types/kudos_program";
+import fs from 'fs'
+import { PublicKey, SystemProgram, Keypair } from '@solana/web3.js';
 
 describe("kudos-program", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,57 @@ describe("kudos-program", () => {
 
   const program = anchor.workspace.KudosProgram as Program<KudosProgram>;
 
-  it("Is initialized!", async () => {
+  // Load local test wallet.
+  const kps = fs.readFileSync("/Users/ashwin/.config/solana/id.json", {encoding: 'utf-8'});
+  const kpb = Buffer.from(JSON.parse(kps));
+  const userWallet = Keypair.fromSecretKey(kpb);
+
+  // Coleague's wallet
+  const colleagueWallet = Keypair.generate();
+
+  it("Initializes Kudos Account", async () => {
+    // Create a PDA
+    const [userStatsPDA, pda_bump] = await PublicKey.findProgramAddress(
+      [ anchor.utils.bytes.utf8.encode("user-stats"),
+        userWallet.publicKey.toBuffer() ],
+        program.programId
+    )
+
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+        .createUserStats("Ashwin", pda_bump)
+        .accounts({
+          user: userWallet.publicKey,
+          userStats: userStatsPDA,
+          systemProgram: SystemProgram.programId
+        })
+        .signers([userWallet])
+        .rpc();
     console.log("Your transaction signature", tx);
+    const res = await program.account.userStats.fetch(userStatsPDA);
+    console.log(res)
+  });
+
+  it("Give Kudos", async () => {
+    // Create a PDA
+    const [userStatsPDA, pda_bump] = await PublicKey.findProgramAddress(
+      [ anchor.utils.bytes.utf8.encode("user-stats"),
+        userWallet.publicKey.toBuffer() ],
+        program.programId
+    )
+
+    // Add your test here.
+    const tx = await program.methods
+        .giveKudos(new anchor.BN(10))
+        .accounts({
+          kudosSender: colleagueWallet.publicKey,
+          kudosReceiver: userWallet.publicKey,
+          userStats: userStatsPDA
+        })
+        .signers([colleagueWallet])
+        .rpc();
+    console.log("Your transaction signature", tx);
+    const res = await program.account.userStats.fetch(userStatsPDA);
+    console.log(res)
   });
 });
